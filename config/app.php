6fd81459b1217d5babb340f4e5eabe715ccc24cd
Fairuznp <farsaungu99@gmail.com>
2025-07-22 05:50:57 +0700

    feat : form-cart

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Jackiedo\Cart\CartServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -183,6 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Cart' => Jackiedo\Cart\Facades\Cart::class,
     ])->toArray(),
 
 ];
