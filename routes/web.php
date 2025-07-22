63c7b94b0b780e9cfcc1396f7d2bad2ec7355571
Fairuznp <farsaungu99@gmail.com>
2025-07-22 10:09:17 +0700

    feat : add transaksi

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\UserControler;
 use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\StokController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\TransaksiController;
 use App\Models\Kategori;
 use App\Models\Pelanggan;
 
@@ -30,6 +32,19 @@ Route::view('login', 'auth.login')->name('login')->middleware('guest');
 Route::post('login', [AuthController::class, 'login'])->middleware('guest');
 Route::post('logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 Route::middleware('auth')->group(function () {
+    Route::get('transaksi/produk', [TransaksiController::class, 'produk'])
+        ->name('transaksi.produk');
+
+    Route::get('transaksi/pelanggan', [TransaksiController::class, 'pelanggan'])
+        ->name('transaksi.pelanggan');
+    Route::get('transaksi/{transaksi}/cetak', [TransaksiController::class, 'cetak'])
+        ->name('transaksi.cetak');
+    Route::post('transaksi/{transaksi}', [TransaksiController::class, 'addPelanggan'])
+        ->name('transaksi.pelanggan.add');
+    Route::resource('transaksi', TransaksiController::class)->except('edit', 'update');
+    Route::get('cart/clear', [CartController::class, 'clear'])->name('cart.clear');
+    Route::resource('cart', CartController::class)->except('create', 'show', 'edit')
+        ->parameters(['cart' => 'hash']);
     Route::resource('user', UserControler::class)->middleware('can:admin');
     Route::singleton('profile', ProfileController::class);
     Route::resource('pelanggan', PelangganController::class);
